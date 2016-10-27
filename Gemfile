36796b05a2c144676d9806ddeef5789d2d228cd1
teiajamese <teiaatkins@gmail.com>
2016-10-27 17:03:17 -0400

    set up application endpoints and versionsing

--- Gemfile
+++ Gemfile
@@ -52,5 +52,10 @@ group :development do
   gem 'sqlite3'
 end
 
+group :test do
+  gem "rspec-rails", "~> 2.14"
+  gem "factory_girl_rails"
+  gem 'ffaker'
+end
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
+require 'api_constraints'
 MarketPlaceApi::Application.routes.draw do
 	#APi definition
 	namespace :api, defaults: {format: :json}, constraints: {subdomain: 'api'}, path: '/' do
+		scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
 		#resources go here
+		end
 	end
 end

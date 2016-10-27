46f0046a665423436c6e093b2cbf56fdbb72c808
teiajamese <teiaatkins@gmail.com>
2016-10-27 16:29:57 -0400

    Set the routes contraints for api

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 MarketPlaceApi::Application.routes.draw do
-
+	#APi definition
+	namespace :api, defaults: {format: :json}, constraints: {subdomain: 'api'}, path: '/' do
+		#resources go here
+	end
 end
